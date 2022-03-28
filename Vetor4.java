6d5d676fef959f85c15f2720a8c3698bf7c364a5
Matheus Muccio <matheus_muccio@outlook.com>
2022-03-28 19:08:51 +0000

    Salvando

---
+++ Vetor4.java
@@ -0,0 +1,25 @@
+import java.util.Random;
+
+
+public class Vetor4 {
+    public static void main(String[] args) {
+        
+
+        Random random = new Random();
+        int numero [] = {random.nextInt(100), random.nextInt(100), random.nextInt(100), random.nextInt(100), random.nextInt(100)};
+
+        
+        for (int index = 0; index < numero.length; index++) {
+            if (numero[index] % 2 == 0){
+                System.out.println("Pares");
+                System.out.println(numero[index]);
+            }
+            else{
+                System.out.println("ímpares");
+                System.out.println(numero[index]);
+            }
+            
+        }
+        
+    }
+}
